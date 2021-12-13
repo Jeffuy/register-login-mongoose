92f58aafeeffb77de6fb32328202d6fd24b12326
Jeff Free <jefffreelance3@gmail.com>
2021-12-13 01:53:36 -0300

    :lock: hashes for passwords

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,83 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+// User model
+
+const User = require("../models/User");
 
 //Login Page
-router.get('/login', (req, res) => res.render('login'));
+router.get("/login", (req, res) => res.render("login"));
 
 //Register Page
-router.get('/register', (req, res) => res.render('Register'));
+router.get("/register", (req, res) => res.render("Register"));
 
 //Register Handle
 
-router.post('/register', (req, res) => {
-	const { name, email, password, password2 } = req.body;
-	let errors = [];
+router.post("/register", (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
 
-	// Check required fields
-	if (!name || !email || !password || !password2) {
-		errors.push({ msg: 'Please fill in all fields' });
-	}
+    // Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: "Please fill in all fields" });
+    }
 
-	// Check passwords match
-	if (password != password2) {
-		errors.push({ msg: 'Passwords do not match' });
-	}
+    // Check passwords match
+    if (password != password2) {
+        errors.push({ msg: "Passwords do not match" });
+    }
 
-	// Check pass length
-	if (password.length < 6) {
-		errors.push({ msg: 'Password should be at least 6 characters' });
-	}
+    // Check pass length
+    if (password.length < 6) {
+        errors.push({ msg: "Password should be at least 6 characters" });
+    }
 
-	if (errors.length > 0) {
-		res.render('register', {
-			errors,
-			name,
-			email,
-			password,
-			password2
-		});
-	} else {
-		res.send('pass');
-	}
+    if (errors.length > 0) {
+        res.render("register", {
+            errors,
+            name,
+            email,
+            password,
+            password2,
+        });
+    } else {
+        // Validation passed
+        User.findOne({ email: email }).then((user) => {
+            if (user) {
+                // User exists
+                errors.push({ msg: "Email already exists" });
+                res.render("register", {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2,
+                });
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password,
+                });
 
+                // Hash password
+                bcrypt.genSalt(10, (err, salt) =>
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+						// Set password to hashed
+                        newUser.password = hash;
+						// Save user
+						newUser.save()
+							.then(user => {
+								res.redirect('/users/login')
+							})
+							.catch(err => console.log(err));
+                    })
+                );
+            }
+        });
+    }
 });
-
 
 module.exports = router;
